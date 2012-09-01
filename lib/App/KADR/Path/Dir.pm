50ab1646d36bbed94149f42ed14b6afb51d9f1dc
Kulag <g.kulag@gmail.com>
2012-09-01 04:41:48 +0000

    Add documentation for Path::Dir and Path::File

--- lib/App/KADR/Path/Dir.pm
+++ lib/App/KADR/Path/Dir.pm
@@ -35,3 +35,56 @@ sub subsumes {
 }
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+=head1 NAME
+
+App::KADR::Path::Dir - like L<Path::Class::Dir>, but faster
+
+=head1 DESCRIPTION
+
+App::KADR::Path::Dir is an optimized and memoized subclass of
+L<Path::Class::Dir>.
+
+=head1 METHODS
+
+App::KADR::Path::Dir inherits all methods from L<Path::Class::Dir> and
+implements the following new ones.
+
+=head2 C<file_class>
+
+	my $file_class = $dir->file_class;
+
+File class in use by this dir.
+
+=head2 C<is_absolute>
+
+	my $is_absolute = $dir->is_absolute;
+
+Check if dir is absolute. This method is memoized.
+
+=head2 C<relative>
+
+	my $relative_dir = $dir->relative;
+	my $relative_dir = $dir->relative('..');
+
+Turn dir into a dir relative to another dir.
+The other dir defaults to the current directory.
+
+=head2 C<stringify>
+
+	my $string = $dir->stringify;
+	my $string = $dir . '';
+
+Turn dir into a string. This method is memoized.
+
+=head2 C<subsumes>
+
+	my $subsumes = $dir->subsumes(dir());
+	my $subsumes = $dir->subsumes(file('foo'));
+
+Check if another dir or file is logically contained within this dir.
+This method is memoized.
+
+=head1 SEE ALSO
+
+	L<App::KADR::Path::File>, L<Path::Class::Dir>
--- lib/App/KADR/Path/File.pm
+++ lib/App/KADR/Path/File.pm
@@ -23,3 +23,48 @@ sub stringify {
 }
 
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
+
+=head1 NAME
+
+L<App::KADR::Path::File> - like L<Path::Class::File>, but faster
+
+=head1 DESCRIPTION
+
+L<App::KADR::Path::File> is an optimized and memoized subclass of
+L<Path::Class::File>.
+
+=head1 METHODS
+
+L<App::KADR::Path::File> inherits all methods from L<Path::Class::File> and
+implements the following new ones.
+
+=head2 C<dir_class>
+
+	my $dir_class = $file->dir_class;
+
+Dir class in use by this file.
+
+=head2 C<is_absolute>
+
+	my $is_absolute = $file->is_absolute;
+
+Check if file is absolute. This method is memoized.
+
+=head2 C<relative>
+
+	my $relative_file = $file->relative;
+	my $relative_file = $file->relative('..');
+
+Turn file into a file relative to another dir.
+The other file defaults to the current directory.
+
+=head2 C<stringify>
+
+	my $string = $file->stringify;
+	my $string = $file . '';
+
+Turn file into a string. This method is memoized.
+
+=head1 SEE ALSO
+
+	L<App::KADR::Path::Dir>, L<Path::Class::File>
