cdbaf6b0457be1406e605889c17035f97dacef85
Kovensky <diogomfranco@gmail.com>
2012-10-11 14:15:38 -0300

    Better replacements for windows path filter
    Also document what each replacement character is.

--- lib/App/KADR/Util.pm
+++ lib/App/KADR/Util.pm
@@ -13,7 +13,15 @@ sub pathname_filter {
 }
 
 sub pathname_filter_windows {
-	$_[0] =~ tr{/\\?"<>|:*}{∕￥？”＜＞｜：＊}r;
+	# All non-described replacements are double-width
+	# versions of the original characters.
+	# ∕ U+2215 DIVISION SLASH
+	# ⧵ U+29F5 REVERSE SOLIDUS OPERATOR
+	# ” U+201D RIGHT DOUBLE QUOTATION MARK
+	# ⟨ U+27E8 MATHEMATICAL LEFT ANGLE BRACKET
+	# ⟩ U+27E9 MATHEMATICAL RIGHT ANGLE BRACKET
+	# ❘ U+2758 LIGHT VERTICAL BAR
+	$_[0] =~ tr{/\\?"<>|:*}{∕⧵？”⟨⟩❘∶＊}r;
 }
 
 sub shortest(@) {
