daa6f7d39fff0af73fa1b547c1d34d53281b31a4
Kulag <g.kulag@gmail.com>
2012-10-12 01:42:26 +0000

    moose: Let FEATURE_VERSION be a public constant

--- lib/App/KADR/Moose.pm
+++ lib/App/KADR/Moose.pm
@@ -9,7 +9,7 @@ use true;
 
 use common::sense;
 
-my $FEATURE_VERSION = ':5.14';
+sub FEATURE_VERSION() { ':5.14' }
 
 my ($moose_import) = Moose::Exporter->setup_import_methods(
 	with_meta => [qw(has)],
@@ -52,7 +52,7 @@ sub import {
 	common::sense->import;
 
 	# Require a perl version.
-	feature->import($FEATURE_VERSION);
+	feature->import(FEATURE_VERSION);
 
 	# Cleanliness
 	namespace::autoclean->import(-cleanee => $into);
