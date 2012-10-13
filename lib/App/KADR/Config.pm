0ed91d253cbf33e5f46ef2faa18a1c050ae378fe
Kulag <g.kulag@gmail.com>
2012-10-13 03:41:23 +0000

    config: Aesthetics

--- lib/App/KADR/Config.pm
+++ lib/App/KADR/Config.pm
@@ -1,15 +1,12 @@
 package App::KADR::Config;
+use App::KADR::Moose;
+use App::KADR::Path 'dir';
 use Class::Load qw(load_optional_class);
 use File::HomeDir;
 use List::MoreUtils qw(all);
-use Moose;
 use Moose::Util::TypeConstraints;
 
-use App::KADR::Path 'dir';
-
 with qw(MooseX::Getopt MooseX::SimpleConfig);
-
-my $appdir = dir(File::HomeDir->my_home)->subdir('.kadr');
 
 subtype 'Collator', as 'CodeRef';
 coerce 'Collator', from 'Str', via {
@@ -58,50 +55,48 @@ coerce 'ExistingDir' => from 'Str' => via { dir($_)->absolute };
 subtype 'ExistingDirs', as 'ArrayRef[ExistingDir]' => where { all { -d $_ } @$_ };
 coerce 'ExistingDirs', from 'ArrayRef', via { [map { dir($_)->absolute } @$_] };
 
+my $appdir   = dir(File::HomeDir->my_home)->subdir('.kadr');
+my $database = 'dbi:SQLite:' . $appdir->file('db');
+
 my @default_config_files
 	= map { $_->stringify } grep { $_->is_file_exists }
 		$appdir->file('config.yml'), $appdir->file('login.yml');
-
 has '+configfile',
 	default => sub{ [@default_config_files] },
 	documentation => 'Default: ~/.kadr/config.yml';
 
-has 'avdump' => (is => 'rw', isa => 'Str', predicate => 'has_avdump', documentation => "Commandline to run avdump.");
-has 'avdump_timeout' => (is => 'rw', isa => 'Int', default => 30, documentation => "How many seconds to wait for avdump to contact AniDB before retrying.");
-has [qw(anidb_username anidb_password)] => (is => 'rw', isa => 'Str', required => 1);
-has 'cache_timeout_mylist_unwatched' => (is => 'rw', isa => 'Int', required => 1, default => 2*60*60);
-has [qw(cache_timeout_file cache_timeout_mylist_watched)] => (is => 'rw', isa => 'Int', required => 1, default => 12*24*60*60);
-
-has 'collator',
-	coerce => 1,
-	default => 'auto',
-	is => 'rw',
-	isa => 'Collator';
-
-has 'database',
-	default => 'dbi:SQLite:' . $appdir->file('db'),
-	is => 'rw',
+has 'avdump',
+	documentation => 'Commandline to run avdump.',
 	isa => 'Str',
-	required => 1;
+	predicate => 'has_avdump';
 
-has 'delete_empty_dirs_in_scanned' => (is => 'rw', isa => 'Str', required => 1, default => 1);
+has 'avdump_timeout',
+	default => 30,
+	documentation => 'Seconds to wait for avdump to contact AniDB before retrying.',
+	isa => 'Int';
+
+has [qw(anidb_username anidb_password)], isa => 'Str', required => 1;
+
+has 'cache_timeout_file',             default => 12*24*60*60, isa => 'Int';
+has 'cache_timeout_mylist_unwatched', default =>     2*60*60, isa => 'Int';
+has 'cache_timeout_mylist_watched',   default => 12*24*60*60, isa => 'Int';
+has 'collator',          coerce => 1, default => 'auto',      isa => 'Collator';
+has 'database',                       default => $database,   isa => 'Str';
+has 'delete_empty_dirs_in_scanned',   default => 1,           isa => 'Str';
 
 has [qw(dir_to_put_unwatched_eps dir_to_put_watched_eps)],
 	coerce => 1,
-	is => 'rw',
 	isa => 'ExistingDir',
 	required => 1;
 
 has [qw(dirs_to_scan valid_dirs_for_unwatched_eps valid_dirs_for_watched_eps)],
 	coerce => 1,
-	is => 'rw',
 	isa => 'ExistingDirs',
 	required => 1;
 
 has 'expire_cache',
 	default => 1,
 	documentation => "DEBUG OPTION. Negate to prevent deleting old cached records.",
-	is => 'rw',
 	isa => 'Bool';
 
 has 'file_naming_scheme',
@@ -121,33 +116,36 @@ has 'file_naming_scheme',
 : if $group_short_name != 'raw' { print ' [' ~ $group_short_name ~ ']' }
 .<: $file_type :>
 EOF
-	is => 'rw',
-	isa => 'Str',
-	required => 1;
+	isa => 'Str';
 
-has 'load_local_cache_into_memory' => (is => 'rw', isa => 'Bool', default => 1, documentation => "Disable to reduce memory usage when doing a longer run. About 15 times faster when kadr doesn't have to talk to anidb.");
+has 'load_local_cache_into_memory',
+	default => 1,
+	documentation => q{Disable to reduce memory usage when doing a longer run. About 15 times faster when kadr doesn't have to talk to anidb.},
+	isa => 'Bool';
 
 has 'query_attempts',
 	default => -1,
 	documentation => 'Number of times to retry a query after a timeout. Default: Unlimited',
-	is => 'rw',
 	isa => 'Int';
 
 has 'query_timeout',
 	default => 15.0,
 	documentation => 'Minimum time to wait for a response to a query. Default: 15.0 seconds',
-	is => 'rw',
 	isa => 'Num';
 
 has 'test',
 	default => 0,
 	documentation => 'Test mode, make no changes.',
-	is => 'rw',
 	isa => 'Bool';
 
-has 'time_to_sleep_when_busy' => (is => 'rw', isa => 'Int', required => 1, default => 10*60, documentation => "How long (in seconds) to sleep if AniDB informs us it's too busy to talk to us.");
-has 'update_anidb_records_for_deleted_files' => (is => 'rw', isa => 'Bool', required => 1, default => 0);
-has 'windows_compatible_filenames' => (is => 'rw', isa => 'Bool', required => 1, default => 0, documentation => 'Default: false. Set to true to make Windows not shit bricks.');
+has 'time_to_sleep_when_busy',
+	default => 10*60,
+	documentation => q{How long (in seconds) to sleep if AniDB informs us it's too busy to talk to us.},
+	isa => 'Int';
 
-__PACKAGE__->meta->make_immutable;
-1;
+has 'update_anidb_records_for_deleted_files', default => 0, isa => 'Bool';
+
+has 'windows_compatible_filenames',
+	default => 0,
+	documentation => 'Default: false. Set to true to make Windows not shit bricks.',
+	isa => 'Bool';
